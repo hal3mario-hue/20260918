4fb0bb257a1aff5f4a2f5cc68e55735cd62d6374
hal <hal3mario@icloud.com>
2026-09-18 17:05:34 +0900

    軽減税率対応

---
+++ function.py
@@ -0,0 +1,13 @@
+def add_tax(price,taxrate=0.1):
+    return int(price + price * taxrate)
+
+
+
+
+
+
+if __name__ == "__main__":
+    # assert：==の結果がTrueだと何も返さない
+    assert add_tax(1000) == 1100
+    assert add_tax(1000,0.08) == 1080
+    print("test")
